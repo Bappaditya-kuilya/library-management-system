3aa77c3bfebd2674d35dcd5bd0706d0ef8280665
Bappaditya Kuilya <bappadityakuilya@gmail.com>
2026-03-26 23:00:38 +0530

    Create bookController.js

---
+++ controllers/bookController.js
@@ -0,0 +1,17 @@
+// Controller for book-related operations
+
+class BookController {
+    // Method to get all books
+    getAllBooks(req, res) {
+        // Logic to retrieve all books
+    }
+    
+    // Method to get a book by ID
+    getBookById(req, res) {
+        // Logic to retrieve a book by ID
+    }
+    
+    // Additional methods can be added here
+}
+
+module.exports = new BookController();
