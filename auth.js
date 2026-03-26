8cf0d1bf25963240ba2c3e33637e4ad857c5955c
Bappaditya Kuilya <bappadityakuilya@gmail.com>
2026-03-26 23:00:47 +0530

    Add auth middleware

---
+++ auth.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+
+// Middleware to authenticate user JSON Web Token
+module.exports = (req, res, next) => {
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ msg: 'Token is not valid' });
+    }
+};
