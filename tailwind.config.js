d684a72d412f4930453010403032a90f61b0e58f
Bappaditya Kuilya <bappadityakuilya@gmail.com>
2026-03-26 22:59:07 +0530

    Create Tailwind CSS configuration file for the frontend

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/**
+ * Tailwind CSS configuration file
+ *
+ * @type {import('tailwindcss').Config}
+ */
+
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
